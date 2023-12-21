882ceef8a6d8fded8c5a5785e12a453246d51154
Pepo Banchev <banchevpepo@gmail.com>
2023-12-21 22:28:10 +0200

    Exercises update

---
+++ ReplaceOperator/replaceOperator.sql
@@ -0,0 +1,19 @@
+-- Task 1: Write a SQL REPLACE statement that inserts two new orders with the following details:
+-- Order 9 data:
+-- OrderID = 9, 
+-- ClientID = "Cl1", 
+-- ProductID = "P1", 
+-- Quantity = 10, 
+-- Cost = 5000
+
+-- Order 10 data:
+-- OrderID = 10, 
+-- ClientID = "Cl2", 
+-- ProductID = "P2", 
+-- Quantity = 5, 
+-- Cost = 100
+-- Once you have executed the SQL statement, you can select all available data in the Orders table. 
+
+-- Task 2: Lucky Shrub have noticed that the cost of order number 9 is $5000. This is a mistake.
+--The order must cost $500. You must help them to change it to $500 by writing a relevant REPLACE statement. 
+-- Once you have executed the SQL statement, select all available data from the Orders table. 
